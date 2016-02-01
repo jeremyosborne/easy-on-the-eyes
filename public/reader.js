fbb8de3efdf2e5fb6ba6b0a3ec3367ae9bbe92e1
Jeremy Osborne <jeremywosborne@gmail.com>
2016-01-31 23:40:09 -0800

    Update.

---
+++ public/reader.js
@@ -0,0 +1,51 @@
+/* jshint browser:true */
+/* global marked:false */
+
+(function(bootstrap) {
+
+    var Content = function(c) {
+        return {
+            toHTML: function() {
+                return marked(c || "");
+            }
+        };
+    };
+    // We only have one content object for now.
+    var content = Content(bootstrap.content);
+
+
+
+    var render = function(content) {
+        document.querySelector(".content").innerHTML = content.toHTML();
+    };
+    // First render.
+    render(content);
+
+
+
+    // Attempt to read links in this single pane view.
+    // We hope whatever links are left are worth reading.
+    var linkInterceptor = function(ev) {
+        var t = ev.target;
+        if (t.tagName.toLowerCase() === "a") {
+            ev.preventDefault();
+            window.location.href = "/?u=" + encodeURIComponent(t.href);
+        }
+    };
+    document.querySelector("body").addEventListener("click", linkInterceptor);
+
+
+
+    var nav = function(content) {
+        var navForm = document.querySelector(".nav");
+        if (!content) {
+            navForm.className = navForm.className.replace(/hide/, "");
+            navForm.addEventListener("submit", function(ev) {
+                console.log("TODO: travel to this URL:", ev.target.querySelector("#u").value);
+                ev.preventDefault();
+            });
+        }
+    };
+    nav(content.toHTML());
+
+})(window.bootstrap);
