1ea814348e3e0096a412f6d1bb6c084626734e17
Jeremy Osborne <jeremywosborne@gmail.com>
2017-01-15 09:35:55 -0800

    Comment update.

--- client/content-sagas.js
+++ client/content-sagas.js
@@ -2,6 +2,7 @@ import * as contentAPI from './content-api'
 import {FETCH_CONTENT, FETCHED_CONTENT, UPDATE_CONTENT} from './content-actions'
 import {put, call, takeEvery} from 'redux-saga/effects'
 
+// Process of requesting content from server and updating content object on page.
 const fetchContent = function* (action) {
   // TODO: Differentiate between an actual error on the content grab and just no
   // content to show.
