93b2964621894b58738a8784100d9877bcb4c23a
Jeremy Osborne <jeremywosborne@gmail.com>
2017-03-27 23:37:01 -0700

    Chores.

--- client/content-reducers.js
+++ client/content-reducers.js
@@ -1,7 +1,7 @@
 import { FETCH_CONTENT, FETCHED_CONTENT } from './content-actions'
 import { genContent } from 'easy-on-the-eyes-content'
 
-export const content = function (state = {}, action) {
+export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_CONTENT:
       return {
--- client/store-reducers.js
+++ client/store-reducers.js
@@ -1,9 +1,9 @@
 //
 // Integration of reducers for redux store.
 //
-import { content } from './content-reducers'
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import content from './content-reducers'
+import {combineReducers} from 'redux'
+import {routerReducer} from 'react-router-redux'
 
 const reducers = combineReducers({
   content,
