18b63c1b8ba629d4503ad1bf0e6d3a5b3e31989b
Jeremy Osborne <jeremywosborne@gmail.com>
2017-05-07 08:34:38 -0700

    Fix relative content pathing.

--- client/content/index.js
+++ client/content/index.js
@@ -28,27 +28,34 @@ export const reducer = (state = DEFAULT_STATE, action) => {
 }
 export default reducer
 
-// Helps with deciding if the link is a url local to our current location or
-// an external url.
+// Determine what is our base url for our page for links to relative references
+// since our "page" is mainly determined by the `?url=` query parameter and not
+// the domain hosting easy-on-the-eyes.
 const contentUrl = () => {
-  var cUrl = url.parse(window.location.href, true).query.url || ''
-  if (cUrl) {
+  const pageUrl = url.parse(window.location.href, true)
+  // content-url
+  let contentUrl = (pageUrl.query && pageUrl.query.url) || ''
+  if (contentUrl) {
     try {
-      cUrl = decodeURIComponent(contentUrl)
+      contentUrl = decodeURIComponent(contentUrl)
     } catch (err) {
-      cUrl = ''
+      // Just in case even with the checks above it is garbage.
+      contentUrl = ''
     }
   }
-  return cUrl
+  return contentUrl
 }
 
 // Transition to content viewing page and request new content.
 export const viewContent = ({href}) => {
   return (dispatch) => {
+    // Fix hrefs with paths relative to the reading content before passing
+    // on to API.
+    href = url.resolve(contentUrl(), href)
     history.push({
       pathname: '/content',
       query: {
-        url: url.resolve(contentUrl(), href)
+        url: href
       }
     })
     // Get new content based on state of url.
